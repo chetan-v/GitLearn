37c3a69c120cbbe566426a11b34c9c8c2d9fdf15
chetan verma <chetanverma932000@gmail.com>
2020-06-22 12:11:29 +0530

    Everything was fine just for testing

--- hello.java
+++ hello.java
@@ -1,7 +1,18 @@
+import java.util.*;
 public class hello{
 	public static void main(String[] args){
 
-        System.out.println("hello its me");
+
+
+		Scanner sc = new Scanner(System.in);
+
+       
+		System.out.println("Enter two number");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+	       	System.out.println("The sum and multiplication is "+(a+b)+" "+(a*b));
+        
+
 }
 
 }
--- hello_world.py
+++ hello_world.py
@@ -5,3 +5,5 @@ print("I am just learning git and github in depth")
 v =  5
 c=v+8
 print(c)
+
+print("Hello I'M doing this again ")
