660aa8581e212768f33b49f1a76f0ecd504054b8
chetan verma <chetanverma932000@gmail.com>
2020-06-22 12:11:29 +0530

    Add some mathematical expression in dev

--- hello.java
+++ hello.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import java.util.*;
 public class hello{
 	public static void main(String[] args){
@@ -20,6 +21,23 @@ public class hello{
 
         System.out.println("hello its me");
 >>>>>>> Add the java file to test
+=======
+import java.util.*;
+public class hello{
+	public static void main(String[] args){
+
+
+
+		Scanner sc = new Scanner(System.in);
+
+       
+		System.out.println("Enter two number");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+	       	System.out.println("The sum and multiplication is "+(a+b)+" "+(a*b));
+        
+
+>>>>>>> Add some mathematical expression in dev
 }
 
 }
