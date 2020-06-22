e05727cdc501013ace98a165239cb9a9d73ec47a
chetan verma <chetanverma932000@gmail.com>
2020-06-22 11:58:51 +0530

    Add some mathematical expression in this file

--- hello_world.py
+++ hello_world.py
@@ -5,5 +5,8 @@ print("I am just learning git and github in depth")
 v =  5
 c=v+8
 print(c)
+<<<<<<< HEAD
 
 print("Hello I'M doing this again ")
+=======
+>>>>>>> Add some mathematical expression in this file
