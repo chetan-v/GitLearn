eaa6d649fac2cb7cdeec33b9e7af3c0c87532fd5
chetan verma <chetanverma932000@gmail.com>
2020-06-22 12:23:32 +0530

    new changes in hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -6,7 +6,12 @@ v =  5
 c=v+8
 print(c)
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 print("Hello I'M doing this again ")
 =======
 >>>>>>> Add some mathematical expression in this file
+=======
+
+print("Hello I'M doing this again ")
+>>>>>>> new changes in hello_world.py
