1416dd103e7d7023832560e40573a426fdfba2d8
chetan verma <chetanverma932000@gmail.com>
2020-06-22 11:48:49 +0530

    Add the java file to test
    new changes in hello_world.py
    
    Create README.md
    
    It is created for personal Learning

---
+++ hello.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class hello{
+	public static void main(String[] args){
+
+
+
+		Scanner sc = new Scanner(System.in);
+
+       
+		System.out.println("Enter two number");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+	       	System.out.println("The sum and multiplication is "+(a+b)+" "+(a*b));
+        
+
+}
+
+}
+
+
--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
 print("its me chetan")
+
+print("I am just learning git and github in depth")
+
+v =  5
+c=v+8
+print(c)
+
+print("Hello I'M doing this again ")
