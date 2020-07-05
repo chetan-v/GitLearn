684599427355c1175caf9858b92b8dce4c8feef4
Chetan Verma <63091769+chetan-v@users.noreply.github.com>
2020-07-05 07:58:43 +0530

    Add commit for understanding (#2)
    * Add the java file to test
    
    new changes in hello_world.py
    
    Create README.md
    
    It is created for personal Learning
    
    * Add commit for understanding
    
    * Modified python
    
    * Remove changes
    
    * Improve Readme file
    
    * Add variable for addition
    
    * Add the java file to test
    
    * Add some mathematical expression in this file
    
    * Add some mathematical expression in dev
    
    * new changes in hello_world.py
    
    * Everything was fine just for testing
    
    * Make everything clear and Understandable
    
    Add variable for addition

--- hello.java
+++ hello.java
@@ -1,7 +1,44 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c59f22743edbed1b0e97962c7de5be88b3315853
 import java.util.*;
 public class hello{
 	public static void main(String[] args){
 
+
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("trying to understand git rebase");
+       
+		System.out.println("Enter two number");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a+b;
+	       	System.out.println("The sum and multiplication is "+ c +" "+(a*b));
+        
+
+<<<<<<< HEAD
+=======
+public class hello{
+	public static void main(String[] args){
+
+        System.out.println("hello its me");
+>>>>>>> Add the java file to test
+=======
+import java.util.*;
+public class hello{
+	public static void main(String[] args){
+
+=======
+import java.util.*;
+public class hello{
+	public static void main(String[] args){
+
+>>>>>>> Everything was fine just for testing
 
 
 		Scanner sc = new Scanner(System.in);
@@ -13,6 +50,12 @@ public class hello{
 	       	System.out.println("The sum and multiplication is "+(a+b)+" "+(a*b));
         
 
+<<<<<<< HEAD
+>>>>>>> Add some mathematical expression in dev
+=======
+>>>>>>> Everything was fine just for testing
+=======
+>>>>>>> c59f22743edbed1b0e97962c7de5be88b3315853
 }
 
 }
--- hello_world.py
+++ hello_world.py
@@ -5,5 +5,28 @@ print("I am just learning git and github in depth")
 v =  5
 c=v+8
 print(c)
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+  
+>>>>>>> Make everything clear and Understandable
 
 print("Hello I'M doing this again ")
+=======
+>>>>>>> Add some mathematical expression in this file
+=======
+
+print("Hello I'M doing this again ")
+>>>>>>> new changes in hello_world.py
+=======
+
+print("Hello I'M doing this again ")
+>>>>>>> Everything was fine just for testing
+=======
+  
+
+print("Hello I'M doing this again ")
+>>>>>>> c59f22743edbed1b0e97962c7de5be88b3315853
